3ecda575e2b7ea5faf3eb2d13713d6c7a9d19f4b
BrodyBroo <149398055+BrodyBroo@users.noreply.github.com>
2024-05-17 12:22:33 -0400

    Update model_setup.py

--- scripts/model_setup.py
+++ scripts/model_setup.py
@@ -1,1 +1,5 @@
-
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+def load_model_and_tokenizer(model_name ='gpt2'):
+  tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+  model = GPT2LMHeadModel.from_pretrained(model_name)
+  return model, tokenizer
